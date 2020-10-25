1203d2426f5b8ae4ce1252818f01ed60ac765da6
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-10-25 16:14:41 -0300

    Feat add fl_chart

--- lib/ui/android/widgets/iac.widget.dart
+++ lib/ui/android/widgets/iac.widget.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:imc/blocs/iac.bloc.dart';
@@ -67,7 +68,110 @@ class IACWidgetState extends State<IACWidget> {
             this.bloc.result,
             textAlign: TextAlign.center
           ),
+          Stack(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1.70,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ),
+                      color: Color(0xff232d37)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 18.0, left: 12.0, top: 24, bottom: 12),
+                    child: LineChart(
+                    avgData(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
       ]
+    );
+  }
+  LineChartData avgData() {
+    return LineChartData(
+      lineTouchData: LineTouchData(enabled: false),
+      gridData: FlGridData(
+        show: true,
+        drawHorizontalLine: true,
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          getTextStyles: (value) =>
+              const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
+          getTitles: (value) {
+            if (value.toInt() == 0){
+              return 'Ex. Baixa';
+            }
+            else if (value.toInt() == 1){
+              return 'Baixa';
+            }
+            else if (value.toInt() == 2){
+              return 'ideal';
+            }
+            else if (value.toInt() == 3){
+              return 'Moderada';
+            }
+            else if (value.toInt() == 4){
+              return 'Ex. Gordura';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData:
+          FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 4,
+      minY: 5,
+      maxY: 35,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            FlSpot(0, 10),
+            FlSpot(1, 15),
+            FlSpot(2, 19),
+            FlSpot(3, 25),
+            FlSpot(4, 29),
+          ],
+          isCurved: true,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(show: true),
+        ),
+      ],
     );
   }
 }
--- lib/ui/android/widgets/imc.widget.dart
+++ lib/ui/android/widgets/imc.widget.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:imc/blocs/imc.bloc.dart';
@@ -67,7 +68,110 @@ class IMCWidgetState extends State<IMCWidget> {
             this.bloc.result,
             textAlign: TextAlign.center
           ),
+          Stack(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1.70,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ),
+                      color: Color(0xff232d37)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 18.0, left: 12.0, top: 24, bottom: 12),
+                    child: LineChart(
+                    avgData(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
       ]
+    );
+  }
+  LineChartData avgData() {
+    return LineChartData(
+      lineTouchData: LineTouchData(enabled: false),
+      gridData: FlGridData(
+        show: true,
+        drawHorizontalLine: true,
+        getDrawingVerticalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: const Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          getTextStyles: (value) =>
+              const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
+          getTitles: (value) {
+            if (value.toInt() == 0){
+              return 'MAGREZA';
+            }
+            else if (value.toInt() == 1){
+              return 'NORMAL';
+            }
+            else if (value.toInt() == 2){
+              return 'SOBREPESO';
+            }
+            else if (value.toInt() == 3){
+              return 'OBESIDADE';
+            }
+            else if (value.toInt() == 4){
+              return 'OBESIDADE GRAVE';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData:
+          FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
+      minX: 0,
+      maxX: 4,
+      minY: 15,
+      maxY: 45,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            FlSpot(0, 18.5),
+            FlSpot(1, 24.9),
+            FlSpot(2, 29.9),
+            FlSpot(3, 29.9),
+            FlSpot(4, 40),
+          ],
+          isCurved: true,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(show: true),
+        ),
+      ],
     );
   }
 }
